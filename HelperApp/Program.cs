c3c165bd1ffb63b94f2b0579a19bc4d8155dde35
maxisoft <maxisoft.eu@gmail.com>
2024-11-08 23:51:55 +0100

    Added HelperApp

---
+++ HelperApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WowAHaha.Utils;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Password Encryption Console App!");
+        Console.WriteLine("Please enter a password to encrypt:");
+        var password = Console.ReadLine();
+
+        Console.WriteLine("Please enter an encryption key:");
+        var key = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(key))
+        {
+            Console.WriteLine("Both password and key must be provided. Please try again.");
+            return;
+        }
+
+        var encryptedPassword = DumbDumbEncryption.Encrypt(password, key);
+        Console.WriteLine($"Your encrypted password is: {encryptedPassword}");
+    }
+}
--- WowAHaha/AssemblyInfo.cs
+++ WowAHaha/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WowAHaha.Tests")]
+[assembly: InternalsVisibleTo("HelperApp")]
